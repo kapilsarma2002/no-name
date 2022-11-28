2f22e213841039c2ce050e1dba1a8011a4a3dd13
root <root@DESKTOP-DC7M7A2.localdomain>
2022-11-28 19:52:59 +0530

    routes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,26 @@
 // const express = require('express')
 import express from 'express'
+import router from './router'
+import morgan from 'morgan'
 
-const server = express()
+const app = express()
 
-server.get('/', (req, res) => {
-  console.log('hello from server')
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use((req,res,next)=> {
+  req.secret = 'virat kohli'
+  next()
+})
+
+
+app.get('/', (req, res) => {
+  console.log('hello from app')
   res.status(200)
   res.json({message : 'hello'})
 })
 
-// module.exports = server
-export default server
+app.use('/api', router)
+
+module.exports = app
+// export default app
