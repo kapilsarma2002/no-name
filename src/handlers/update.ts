8f8836ab80c154f6af27c4e9ff214612d5e85a57
“kapilsarma2002” <sarmakapil1818@gmail.com>
2022-12-15 19:01:59 +0530

    handlers-solution

--- src/handlers/update.ts
+++ src/handlers/update.ts
@@ -25,9 +25,10 @@ export const getUpdates = async (req, res) => {
 };
 
 export const createUpdate = async (req, res) => {
+
   const product = await prisma.product.findUnique({
     where: {
-      id: req.body.id,
+      id: req.body.productId
     },
   });
 
@@ -36,7 +37,11 @@ export const createUpdate = async (req, res) => {
   }
 
   const update = await prisma.update.create({
-    data: req.body,
+    data: {
+      title: req.body.title,
+      body: req.body.body,
+      product: {connect: {id: product.id}}
+    },
   });
 
   res.json({ data: update });
--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { body } from "express-validator"
 import { Router } from 'express'
 import { handleInputErrors } from "./modules/middleware"
 import { createProduct, deleteProduct, getOneProduct, getProducts } from "./handlers/product"
+import { createUpdate, deleteUpdate, getOneUpdate, getUpdates, updateUpdate } from "./handlers/update"
 
 const router = Router()
 
@@ -14,22 +15,22 @@ router.put('/product/:id', body('name').isString(), handleInputErrors, (req, res
  router.delete('/product/:id', deleteProduct)
 
 //update
-router.get('/update', () => {})
-router.get('/update/:id', () => {})
+router.get('/update', getUpdates)
+router.get('/update/:id', getOneUpdate)
 router.put('/update/:id',
   body('title').optional(),
   body('body').optional(),
   body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']).optional(),
   body('version').optional(),
-  () => {}
+  updateUpdate
 )
 router.post('/update',
   body('title').exists().isString(),
   body('body').exists().isString(),
   body('productId').exists().isString(),
-  () => {}
+  createUpdate
 )
-router.delete('/update/:id', () => {})
+router.delete('/update/:id', deleteUpdate)
 
 //update point
 router.get('/updatepoint', () => {})
